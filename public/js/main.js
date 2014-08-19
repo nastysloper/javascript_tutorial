993ccdc973f232b6e96a5d1a960dd3cc93e1c16a
Rich Vogt <richvogtjr@gmail.com>
2014-08-19 10:07:07 -0500

    add object and map to js file

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,36 @@
-var human = function(){
-  function speak(){
-    return "Hello, user!";
+$(document).ready(function(){
+  function Human(name){
+    this.name = name;
   }
+
+  mack = new Human("Mack");
+
+  Human.prototype.identify = function(){
+    return "Hi! My name is " + this.name + ".";
+  }
+  console.log(mack.identify());
+
+
+})
+
+
+
+cities = {
+  "Chicago": "Illinois",
+  "New York": "New York",
+  "Tampa": "Florida",
+  "Seattle": "Washington",
+  "Denver": "Colorado",
+  "Dallas": "Texas"
 }
+
+dogMap = {
+  smallDogs: [
+    "Corgie", "Pug"
+  ],
+  largeDogs: [
+    "Dalmation", "St. Bernard"
+  ]
+}
+
+
